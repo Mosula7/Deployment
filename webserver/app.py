9cfbee5fa2b29105ce72e15a90b00fd3427de667
Giorgi Mosulishvili <121322764+Mosula7@users.noreply.github.com>
2024-05-20 05:54:25 +0400

    got webserver to work

--- webserver/app.py
+++ webserver/app.py
@@ -7,11 +7,6 @@ model = cat.CatBoostClassifier()
 model.load_model(os.path.join('models', 'model'))
 
 
-def make_prediction(data):
-    result = model.predict_proba(data)[:, -1]
-    return result
-
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -21,36 +16,66 @@ def home():
 def predict():
     if request.method == 'POST':
         form_data = {
-            'gender': request.form['gender'],
-            'senior_citizen': request.form['senior_citizen'],
-            'partner': request.form['partner'],
-            'dependents': request.form['dependents'],
-            'tenure': request.form['tenure'],
-            'phone_service': request.form['phone_service'],
-            'multiple_lines': request.form['multiple_lines'],
-            'internet_service': request.form['internet_service'],
-            'online_security': request.form['online_security'],
-            'online_backup': request.form['online_backup'],
-            'device_protection': request.form['device_protection'],
-            'tech_support': request.form['tech_support'],
-            'streaming_tv': request.form['streaming_tv'],
-            'streaming_movies': request.form['streaming_movies'],
-            'contract': request.form['contract'],
-            'paperless_billing': request.form['paperless_billing'],
-            'payment_method': request.form['payment_method'],
-            'monthly_charges': request.form['monthly_charges'],
-            'total_charges': request.form['total_charges']
+            'gender': request.form['gender'].lower(),
+            'senior_citizen': request.form['senior_citizen'].lower(),
+            'partner': request.form['partner'].lower(),
+            'dependents': request.form['dependents'].lower(),
+            'tenure': float(request.form['tenure']),
+            'phone_service': request.form['phone_service'].lower(),
+            'multiple_lines': request.form['multiple_lines'].lower(),
+            'internet_service': request.form['internet_service'].lower(),
+            'online_security': request.form['online_security'].lower(),
+            'online_backup': request.form['online_backup'].lower(),
+            'device_protection': request.form['device_protection'].lower(),
+            'tech_support': request.form['tech_support'].lower(),
+            'streaming_tv': request.form['streaming_tv'].lower(),
+            'streaming_movies': request.form['streaming_movies'].lower(),
+            'contract': request.form['contract'].lower(),
+            'paperless_billing': request.form['paperless_billing'].lower(),
+            'payment_method': request.form['payment_method'].lower(),
+            'monthly_charges': float(request.form['monthly_charges']),
+            'total_charges': float(request.form['total_charges'])
         }
         # Make prediction
-        result = make_prediction(list(form_data.values()))
-        return redirect(url_for('result', result=result))
+        binary_columns = [
+            'senior_citizen',
+            'partner',
+            'dependents',
+            'phone_service',
+            'paperless_billing'
+        ]
+
+        data = []
+        for key, value in form_data.items():
+            if key == 'gender':
+                if value == 'female':
+                    data.append(1)
+                if value == 'male':
+                    data.append(0)
+            elif key in binary_columns:
+                if value == 'yes':
+                    data.append(1)
+                if value == 'no':
+                    data.append(0)
+            else:
+                data.append(value)
+
+        probability = round(model.predict_proba([data])[:, -1][0], 4)
+        result = round(probability)
+        return redirect(url_for('result', result=result,
+                                probability=probability))
     return render_template('predict.html')
 
 
 @app.route('/result')
 def result():
     result = request.args.get('result')
-    return render_template('result.html', result=result)
+    probability = request.args.get('probability')
+    background_color = 'green' if int(result) == 1 else 'red'
+    return render_template('result.html',
+                           probability=probability,
+                           result=result,
+                           background_color=background_color)
 
 
 if __name__ == '__main__':
